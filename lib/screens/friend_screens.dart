78e0cd92864d8c3fa2cffc95d99baafb2f8f4bcb
Jeong yh <v230321@gmail.com>
2023-05-18 23:25:06 +0900

    11

--- lib/screens/friend_screens.dart
+++ lib/screens/friend_screens.dart
@@ -23,7 +23,7 @@ class FriendScreen extends StatelessWidget {
                   return SimpleDialog(
                     title: const Text('알림'),
                     children: [
-                      ListTile( ),
+                      ListTile(),
                     ],
                   );
                 },
@@ -40,57 +40,67 @@ class FriendScreen extends StatelessWidget {
               child: Text('친구가 없습니다.'),
             );
           } else {
-            return ListView.builder(
-              itemCount: friends.length,
-              itemBuilder: (context, index) {
-                String friendName = friends[index]['name'] ?? '';
-                String friendImageUrl = friends[index]['imageUrl'] ?? '';
+            return Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: ListView.builder(
+                itemCount: friends.length,
+                itemBuilder: (context, index) {
+                  String friendName = friends[index]['name'] ?? '';
+                  String friendImageUrl = friends[index]['imageUrl'] ?? '';
 
-                return ListTile(
-                   onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: ((context) =>
-                            EventCalendar(friendUid: friends[index]['uid'])),
+                  return Container(
+                    margin: const EdgeInsets.symmetric(vertical: 12.0), // Add vertical margin
+                    child: ListTile(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: ((context) =>
+                                EventCalendar(friendUid: friends[index]['uid'])),
+                          ),
+                        );
+                      },
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(friendImageUrl),
+                        radius: 30,
                       ),
-                    );
-                  },
-                  leading: CircleAvatar(
-                    backgroundImage: NetworkImage(friendImageUrl),
-                  ),
-                  title: Text(friendName),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete, color: Colors.red),
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: const Text('친구 삭제'),
-                            content: const Text('정말로 삭제하시겠습니까?'),
-                            actions: [
-                              TextButton(
-                                onPressed: () async {
-                                  await _profileController.deleteFriend(index);
-                                  Navigator.of(context).pop();
-                                },
-                                child: const Text('예'),
-                              ),
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                                child: const Text('취소'),
-                              ),
-                            ],
+                      title: Text(
+                        friendName,
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(Icons.delete, color: Colors.red),
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text('친구 삭제'),
+                                content: const Text('정말로 삭제하시겠습니까?'),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () async {
+                                      await _profileController.deleteFriend(index);
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: const Text('예'),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: const Text('취소'),
+                                  ),
+                                ],
+                              );
+                            },
                           );
                         },
-                      );
-                    },
-                  ),
-                );
-              },
+                      ),
+                    ),
+                  );
+                },
+              ),
             );
           }
         },
