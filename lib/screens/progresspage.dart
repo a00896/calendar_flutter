42b1390b57f22adf42057bd503104c67203f8973
Cjxisia <100449269+Cjxisia@users.noreply.github.com>
2023-06-06 09:27:37 +0900

    Add files via upload

--- lib/screens/progresspage.dart
+++ lib/screens/progresspage.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 class ProgressPage extends StatefulWidget {
   @override
-  _ProgressPageState createState() => _ProgressPageState();
+  ProgressPageState createState() => ProgressPageState();
 }
 
-class _ProgressPageState extends State<ProgressPage> {
+class ProgressPageState extends State<ProgressPage> {
   List<ProgressData> progressList = [
     ProgressData(name: 'Graph 1', progress: 0.1),
     ProgressData(name: 'Graph 2', progress: 0.3),
@@ -29,9 +29,6 @@ class _ProgressPageState extends State<ProgressPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Progress Page'),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -41,31 +38,46 @@ class _ProgressPageState extends State<ProgressPage> {
               style: TextStyle(fontSize: 24),
             ),
             SizedBox(height: 20),
-
-            for (int i = 0; i < progressList.length; i++) // 수정
-              Column(
-                children: [
-                  Text(
-                    progressList[i].name,
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  SizedBox(height: 5),
-                  ProgressBar(progress: progressList[i].progress),
-                  SizedBox(height: 10),
-                ],
+            Expanded(
+              child: ListView.separated(
+                itemCount: progressList.length,
+                separatorBuilder: (context, index) => SizedBox(height: 10), // 그래프 사이 간격
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20), // 좌우 여백 설정
+                    child: Column(
+                      children: [
+                        Text(
+                          progressList[index].name,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        SizedBox(height: 5),
+                        ProgressBar(progress: progressList[index].progress),
+                      ],
+                    ),
+                  );
+                },
               ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                addProgressData(ProgressData(name: 'New Graph', progress: 0.7));
-              },
-              child: Text('Add Progress'),
             ),
-            ElevatedButton(
-              onPressed: () {
-                removeProgressData();
-              },
-              child: Text('Remove Progress'),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    addProgressData(
+                      ProgressData(name: 'New Graph', progress: 0.7),
+                    );
+                  },
+                  child: Text('Add Progress'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    removeProgressData();
+                  },
+                  child: Text('Remove Progress'),
+                ),
+              ],
             ),
           ],
         ),
@@ -74,17 +86,30 @@ class _ProgressPageState extends State<ProgressPage> {
   }
 }
 
-class ProgressBar extends StatelessWidget {
+  class ProgressBar extends StatelessWidget {
   final double progress;
 
   const ProgressBar({required this.progress});
 
   @override
   Widget build(BuildContext context) {
-    return LinearProgressIndicator(
-      value: progress,
-      backgroundColor: Colors.grey[200],
-      valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+    return Column(
+      children: [
+        Container(
+          width: 1000, // 가로 길이를 최대로 확장
+          height: 20, // 그래프의 높이
+          child: LinearProgressIndicator(
+            value: progress,
+            backgroundColor: Colors.grey[200],
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+          ),
+        ),
+        Text(
+          '(${(progress * 100).toStringAsFixed(1)}% / 100%)',
+          style: TextStyle(fontSize: 15),
+        ),
+        SizedBox(height: 20),
+      ],
     );
   }
 }
