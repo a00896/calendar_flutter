e362d0d63295475d4a81551de723cc1f3821890d
Cjxisia <100449269+Cjxisia@users.noreply.github.com>
2023-06-06 22:17:27 +0900

    Add files via upload

--- lib/screens/progresspage.dart
+++ lib/screens/progresspage.dart
@@ -1,16 +1,82 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ProgressPage extends StatefulWidget {
   @override
-  State<ProgressPage> createState() => _ProgressPageState();
+  ProgressPageState createState() => ProgressPageState();
 }
 
-class _ProgressPageState extends State<ProgressPage> {
+class ProgressPageState extends State<ProgressPage> {
+  var collection_url = 'users/yWzLtNsNz2UrJjvGGq1lmR4aOVv2/calendars';
+  Map<String, List> mySelectedEvents = {};
   List<ProgressData> progressList = [
     ProgressData(name: 'Graph 1', progress: 0.1),
     ProgressData(name: 'Graph 2', progress: 0.3),
     ProgressData(name: 'Graph 3', progress: 0.5),
   ];
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadPreviousEvents();
+    getUserData();
+    getCalendarData();
+  }
+
+  loadPreviousEvents() {
+    mySelectedEvents = {};
+  }
+
+  Future<void> getUserData() async {
+    print('print: getUserData');
+    var user = FirebaseAuth.instance.currentUser;
+    var users = FirebaseFirestore.instance.collection('users');
+    if (FirebaseAuth.instance.currentUser != null) {
+      final uid = user!.uid;
+      var documentSnapshot = await users.doc(uid).get();
+      print(documentSnapshot.data());
+    } else {
+      collection_url = 'users/yWzLtNsNz2UrJjvGGq1lmR4aOVv2/calendars';
+    }
+  }
+
+  Future<void> getCalendarData() async {
+    var user = FirebaseAuth.instance.currentUser;
+    if (FirebaseAuth.instance.currentUser != null) {
+      final uid = user!.uid;
+      collection_url = 'users/$uid/calendars';
+    } else {
+      collection_url = 'users/yWzLtNsNz2UrJjvGGq1lmR4aOVv2/calendars';
+    }
+
+    try {
+      var response = await FirebaseFirestore.instance
+          .collection(collection_url)
+          .get();
+      for (var result in response.docs) {
+        if (mySelectedEvents[result['date']] != null) {
+          mySelectedEvents[result['date']]?.add({
+            'title': result['title'],
+            'desc': result['desc'],
+          });
+        } else {
+          mySelectedEvents[result['date']] = [
+            {
+              'title': result['title'],
+              'desc': result['desc'],
+            }
+          ];
+        }
+        setState(() {});
+      }
+    } on FirebaseException catch (e) {
+      print(e);
+    } catch (error) {
+      print(error);
+    }
+  }
 
   void addProgressData(ProgressData progressData) {
     setState(() {
@@ -28,10 +94,8 @@ class _ProgressPageState extends State<ProgressPage> {
 
   @override
   Widget build(BuildContext context) {
+    print(mySelectedEvents);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Progress Page'),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -41,30 +105,46 @@ class _ProgressPageState extends State<ProgressPage> {
               style: TextStyle(fontSize: 24),
             ),
             SizedBox(height: 20),
-            for (int i = 0; i < progressList.length; i++) // 수정
-              Column(
-                children: [
-                  Text(
-                    progressList[i].name,
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  SizedBox(height: 5),
-                  ProgressBar(progress: progressList[i].progress),
-                  SizedBox(height: 10),
-                ],
+            Expanded(
+              child: ListView.separated(
+                itemCount: progressList.length,
+                separatorBuilder: (context, index) => SizedBox(height: 10), // 그래프 사이 간격
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20), // 좌우 여백 설정
+                    child: Column(
+                      children: [
+                        Text(
+                          progressList[index].name,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        SizedBox(height: 5),
+                        ProgressBar(progress: progressList[index].progress),
+                      ],
+                    ),
+                  );
+                },
               ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                addProgressData(ProgressData(name: 'New Graph', progress: 0.7));
-              },
-              child: Text('Add Progress'),
             ),
-            ElevatedButton(
-              onPressed: () {
-                removeProgressData();
-              },
-              child: Text('Remove Progress'),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    addProgressData(
+                      ProgressData(name: 'New Graph', progress: 0.7),
+                    );
+                  },
+                  child: Text('Add Progress'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    removeProgressData();
+                  },
+                  child: Text('Remove Progress'),
+                ),
+              ],
             ),
           ],
         ),
@@ -73,17 +153,30 @@ class _ProgressPageState extends State<ProgressPage> {
   }
 }
 
-class ProgressBar extends StatelessWidget {
+  class ProgressBar extends StatelessWidget {
   final double progress;
 
   const ProgressBar({required this.progress});
 
   @override
   Widget build(BuildContext context) {
-    return LinearProgressIndicator(
-      value: progress,
-      backgroundColor: Colors.grey[200],
-      valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+    return Column(
+      children: [
+        Container(
+          width: 1000, // 가로 길이를 최대로 확장
+          height: 20, // 그래프의 높이
+          child: LinearProgressIndicator(
+            value: progress,
+            backgroundColor: Colors.grey[200],
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+          ),
+        ),
+        Text(
+          '(${(progress * 100).toStringAsFixed(1)}% / 100%)',
+          style: TextStyle(fontSize: 15),
+        ),
+        SizedBox(height: 20),
+      ],
     );
   }
 }
